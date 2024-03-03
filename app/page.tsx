baffd33eb757ef6629cff3e80b1b35ffaa78b55a
Nguyễn Thanh Tuấn <davidtuan@192.168.1.5>
2024-03-03 18:36:39 +0700

    Add and import scss file

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
-
+import styles from '@/app/ui/home.module.scss'
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
